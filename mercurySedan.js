02add06dd49fcc418c88f35308688ab83a543cc3
sjcowdery96 <sjc.cowdery@gmail.com>
2023-09-10 17:08:46 -0500

    added final embelishments

--- mercurySedan.js
+++ mercurySedan.js
@@ -7,7 +7,49 @@ console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
-//inheritence is neat!
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, mileage, numberOfWheels, passengers, maximumPassengers, maximumSpeed,fuel){
+        super(make, model, year, color, mileage) //calls the parent constructor
+        //adds the new parameters specific to car constructor
+        this.numberOfWheels = numberOfWheels
+        this.passenges = passengers
+        this.maximumPassengers = maximumPassengers
+        this.maximumSpeed = maximumSpeed
+        this.fuel = fuel
+        this.scheduledService = false
+    }
+
+    loadPassenger(numberOfPassengers){
+        if(numberOfPassengers>this.maximumPassengers){
+            console.log("y'all mfs can't fit!")
+        }
+    }
+    //starts the car...vroom!
+    start(){
+        if(this.fuel>0){
+            return true
+            console.log("Vroom!")
+        }
+        else return false
+    }
+    //method for determining scheduled service times
+    scheduledService(mileage){
+        if(mileage>30000){
+            this.scheduledService = true
+        }
+        return this.scheduledService
+    }
+    //adds a method for when you get your car serviced
+    serviced(){
+        if(this.scheduledService == true){
+            this.scheduledService = false
+        }
+    }
+    
+}
+let b = new  Car("Ford", "Taurus", 2011, "blue",12415, 4, 4, 4) //creates a new Ford Taurus 
+
+console.log(b.doors) //prints the doors on the car
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
